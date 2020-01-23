7b74fe8e9816c287d0d224ee30deef6d94432c52
NirobNabil <thetwin11100@gmail.com>
2020-01-24 01:58:15 +0600

    Updates

---
+++ precache-manifest.e6a1e50e83f2929f6bbae2dff5904c40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed74fb3395dddb54bb04a35af7794cfc",
+    "url": "/places-demo/index.html"
+  },
+  {
+    "revision": "dde257c27a53f44089e9",
+    "url": "/places-demo/static/css/2.624e4bf4.chunk.css"
+  },
+  {
+    "revision": "9a8c3d39a7469d562cc7",
+    "url": "/places-demo/static/css/main.6f404bb4.chunk.css"
+  },
+  {
+    "revision": "dde257c27a53f44089e9",
+    "url": "/places-demo/static/js/2.fe1f56e4.chunk.js"
+  },
+  {
+    "revision": "409e88028c833aab3b5c4200318b3af2",
+    "url": "/places-demo/static/js/2.fe1f56e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a8c3d39a7469d562cc7",
+    "url": "/places-demo/static/js/main.2737a222.chunk.js"
+  },
+  {
+    "revision": "c87740f63d27e9aca439",
+    "url": "/places-demo/static/js/runtime-main.617a779b.js"
+  }
+]);
